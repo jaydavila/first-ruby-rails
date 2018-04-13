f8cddd001b0a00382d367a0d753ff9c21b690873
Jay Davila <jaydavila94@icloud.com>
2018-04-13 11:49:11 -0400

    user authin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,18 @@ class SessionsController < ApplicationController
    puts '************'
    puts params.inspect
    puts '*************'
+   # fid a user i the database that matches the username in the form
+   @user = User.find_by(username: params[:session][:username])
+   # check if there is a user with that username, and check if the password is that user's password
+    if @user && @user.password == params(params[:session][:password])
+      puts ' LOGIN SUCCESSFUL'
+      log_in(@user)
+   redirect_to user_path @user
+ else
+   puts "LOGIN FAIlED ..... DUN DUN"
    redirect_to login_path
   end
-
+end
   def destroy
 
   end
